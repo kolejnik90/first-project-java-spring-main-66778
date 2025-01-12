24e578d48e3a6c580d75635b129b5aec7c9091c5
kolejnik90 <olejnik.kacper01@gmail.com>
2025-01-12 23:01:17 +0100

    Initial commit

---
+++ src/main/java/pl/edu/vistula/first_project_java_spring_66778/FirstProjectJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_project_java_spring_66778;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectJavaSpringApplication.class, args);
+	}
+
+}
